3574af2765f870bd4f16101bf0cf4c16384d9a2e
tedmarszalek <tedman.marszalek@gmail.com>
2015-11-12 01:30:17 -0600

    image edits, contact/about pages

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,12 @@
 class WelcomeController < ApplicationController
+	
   def index
   end
+
+  def contact
+  end
+
+  def about
+  end
+
 end
--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
+  test "should get contact" do
+    get :contact
     assert_response :success
   end
 
